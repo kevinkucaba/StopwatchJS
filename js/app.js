a4507cdc3fd6dc70fc21cb9ddb0b947eb2a6273c
Kevin Kucaba <kevinkucaba28@gmail.com>
2025-04-01 18:17:26 -0500

    Identified all needed components by ID

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const startBtn = document.getElementById('start-btn')
+const stopBtn = document.getElementById('stop-btn')
+const clearBtn = document.getElementById('clear-btn')
+const timer = document.getElementById('timer')
